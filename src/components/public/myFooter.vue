004d6a59c4ec0c6a8e48f4adf4f07fa408e7272d
e <573737092@qq.com>
2016-12-07 17:04:23 +0800

    添加前端主代码框架

---
+++ src/components/public/myFooter.vue
@@ -0,0 +1,13 @@
+<template>
+	<h1>这里是Footer，样式和内容找个人写，自己商量人</h1>
+</template>
+
+<script>
+	export default {
+	  name: 'myfooter'
+	}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/public/myHeader.vue
@@ -0,0 +1,13 @@
+<template>
+	<h1>这里是Header，样式和内容找个人写，自己商量人</h1>
+</template>
+
+<script>
+	export default {
+	  name: 'myheader'
+	}
+</script>
+
+<style>
+
+</style>
