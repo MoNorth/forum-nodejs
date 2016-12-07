648c03f72e7410a2b76f21d6220508e272159448
glt__1824@163.com <glt__1824@163.com>
2016-12-07 23:06:58 +0800

    完成myFooter和myHeader组件

--- src/components/public/myHeader.vue
+++ src/components/public/myHeader.vue
@@ -1,13 +1,69 @@
 <template>
-	<h1>这里是Header，样式和内容找个人写，自己商量人</h1>
+  <div class="my_header">
+    <nav class="header_nav">
+      <a class="header_nav_a" :class="{'header_nav_this':item.active}" v-for="item in items">{{item.name}}</a>
+    </nav>
+  </div>
 </template>
-
 <script>
 	export default {
-	  name: 'myheader'
+	  name: 'myheader',
+    data(){
+      return {
+        items : [
+          {
+            name:'热门话题',
+            active:true
+          },
+          {
+            name:'话题分类',
+            active:false
+          },
+          {
+            name:'消息',
+            active:false
+          },
+          {
+            name:'发现',
+            active:false
+          },
+          {
+            name:'个人中心',
+            active:false
+          }
+        ]
+      } 
+    }
 	}
 </script>
-
 <style>
-
+  .my_header {
+    width: 100%;
+    height: 40px;
+    font-weight: bolder;
+    line-height: 40px;
+    background-color: #fff;
+  }
+  .header_nav {
+    width: 60%;
+    margin: 0 auto;
+    font-size: 20px;
+    border-bottom: 2px solid #00BCD4;
+  }
+  .header_nav_a {
+    cursor: pointer;
+    display: inline-block;
+    text-align: center;
+    padding: 0 3%;
+    color: #000;
+    text-decoration: none;
+  }
+  .header_nav_a:hover {
+    color: #fff;
+    background: #00BCD4;
+  }
+  .header_nav_this {    
+    color: #fff;
+    background: #00BCD4;
+  }
 </style>
