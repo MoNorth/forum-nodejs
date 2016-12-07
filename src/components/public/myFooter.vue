037277d50c27e5e2a1ff1f536ed666c1476f2c6c
glt__1824@163.com <glt__1824@163.com>
2016-12-07 23:06:15 +0800

    完成myFooter和myHeader组件

--- src/components/public/myFooter.vue
+++ src/components/public/myFooter.vue
@@ -1,13 +1,41 @@
 <template>
-	<h1>这里是Footer，样式和内容找个人写，自己商量人</h1>
+  <div class="my_footer">
+    <p class="footer_logo">论坛</p>
+    <ul class="footer_right">
+      <li>加入我们</li>
+      <li>建议反馈</li>
+      <li>联系我们</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-	export default {
-	  name: 'myfooter'
-	}
+  export default {
+    name: 'myfooter'
+  }
 </script>
 
 <style>
-
+  .my_footer{
+    width: 60%;
+    margin: 100px auto;
+    background-color: #fff;
+    border-top: 2px solid #00BCD4;
+    padding-top: 20px;
+  }
+  .footer_logo {
+    margin-left: 3%;
+    cursor: pointer;
+    display: inline-block;
+  }
+  .footer_right {
+    color: #999999;
+    list-style-type: none;
+    display: inline-block;
+    float: right;
+  }
+  .footer_right li {
+    cursor: pointer;
+    display: inline-block;
+  }
 </style>
