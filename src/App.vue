789b31027b002b8fd8763c63ffc86949c24a892e
e <573737092@qq.com>
2016-12-10 16:27:17 +0800

    调整了lib加载顺序

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,8 @@
     <myheader></myheader>
 
     <div id="content">
-    hello world<br/>
-    <table class="table">
-          <tr>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-          </tr>
-          <tr>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-          </tr>
-    </table>
+      <classification></classification>
+    
     </div>
 
     <myfooter></myfooter>
@@ -28,13 +17,15 @@
 import myHeader from './components/public/myHeader.vue'
 import myFooter from './components/public/myFooter.vue'
 import Backtop from './components/public/Backtop.vue'
+import Classification from './components/index/Hotpost.vue'
 
 export default {
   name: 'app',
   components: {
     myheader : myHeader,
     myfooter : myFooter,
-    backtop : Backtop
+    backtop : Backtop,
+    classification : Classification
   }
   
 }
