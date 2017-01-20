8e113a229ceefbef51ed5532d1fa45f71e94ac56
glt__1824@163.com <glt__1824@163.com>
2017-01-20 22:52:47 +0800

    修改

--- src/components/public/myHeader.vue
+++ src/components/public/myHeader.vue
@@ -6,9 +6,11 @@
 <template>
   <div class="my_header">
     <nav class="header_nav">
-      <span class="header_logo">Forum</span>
+      <div class="header-navbox">
+        <router-link tag="span" to="/" class="header_logo">Forum</router-link>
+        <div><router-link :to="item.addurl" class="header_nav_a" v-for="item in data.items">{{item.name}}</router-link></div>
+      </div>
       <search></search>
-      <div><a class="header_nav_a" :class="{'header_nav_this':item.active}" v-for="item in data.items" @click="makeActive(item)">{{item.name}}</a></div>
     </nav>
   </div>
 </template>
@@ -18,22 +20,27 @@ import search from './Search.vue'
     items : [
       {
         name:'热门话题',
+        addurl:'/hotpost',
         active:true
       },
       {
         name:'话题分类',
+        addurl:'/classification',
         active:false
       },
       {
         name:'消息',
+        addurl:'/messages',
         active:false
       },
       {
         name:'发现',
+        addurl:'/search',
         active:false
       },
       {
         name:'个人中心',
+        addurl:'/personal',
         active:false
       }
     ]
@@ -67,83 +74,98 @@ import search from './Search.vue'
     line-height: 60px;
     background-color: #3FA46A;
     font-family: '微软雅黑';
+    text-decoration: none;
     nav {
-      min-width: 718px;
+      min-width: 751px;
       width: 60%;
       margin: 0 auto;
       text-align: center;
       font-size: 16px;
-      .header_logo {
-        min-width: 80px;
-        color: @color;
-        font: 30px 'Arial';
-        display: inline-block;
-        vertical-align: top;
-        margin-top: 13px;
-        width: 8%;
-        margin-right: 2%;
-        text-align: center;
-        vertical-align: top;
-      }
-      div {
-        width: 40%;
-        display: inline-block;
-        min-width: 460px;
-        a {
-          padding: 0 4%;
-          height: 40px;
+      .header-navbox {
+        width: 80%;
+        float: left;
+        .header_logo {
+          min-width: 80px;
+          color: @color;
+          font: 30px 'Arial';
           display: inline-block;
-          line-height: 40px;
           vertical-align: top;
+          margin-top: 13px;
+          width: 8%;
+          margin-right: 2%;
           text-align: center;
-          margin: 10px auto;
-          color: #237546;
-          font-weight: 400;
-          -webkit-transition: color 0.3s;
-          -moz-transition: color 0.3s;
-          transition: color 0.3s;
-          position: relative;
-          &::before,&::after {
-            position: absolute;
-            left: 0;
-            width: 100%;
-            height: 2px;
-            background: #fff;
-            content: '';
-            opacity: 0;
-            -webkit-transition: opacity 0.3s, -webkit-transform 0.3s;
-            -moz-transition: opacity 0.3s, -moz-transform 0.3s;
-            transition: opacity 0.3s, transform 0.3s;
-          }
-          &::before {
-            top: 0;
-            -webkit-transform: translateY(-10px);
-            -moz-transform: translateY(-10px);
-            transform: translateY(-10px);
-          }
-          &::after {
-            bottom: 0;
-            -webkit-transform: translateY(10px);
-            -moz-transform: translateY(10px);
-            transform: translateY(10px);
-          }
-          &:hover {
-            color: #fff;
-            cursor: pointer;
-            text-decoration: none;
-          }
-          &:focus {
-            color: #fff;
-          }
-          &:hover::before,&:focus::before,&:hover::after,&:focus::after {
-          opacity: 1;
-          -webkit-transform: translateY(0px);
-          -moz-transform: translateY(0px);
-          transform: translateY(0px);          
-          }
+          vertical-align: top;
+          cursor: pointer;
         }
+        div {
+          width: 40%;
+          display: inline-block;
+          min-width: 460px;
+          a {
+            padding: 0 4%;
+            text-decoration: none;
+            height: 40px;
+            display: inline-block;
+            line-height: 40px;
+            vertical-align: top;
+            text-align: center;
+            margin: 10px auto;
+            color: #237546;
+            font-weight: 400;
+            -webkit-transition: color 0.3s;
+            -moz-transition: color 0.3s;
+            transition: color 0.3s;
+            position: relative;
+            &::before,&::after {
+              position: absolute;
+              left: 0;
+              width: 100%;
+              height: 2px;
+              background: #fff;
+              content: '';
+              opacity: 0;
+              -webkit-transition: opacity 0.3s, -webkit-transform 0.3s;
+              -moz-transition: opacity 0.3s, -moz-transform 0.3s;
+              transition: opacity 0.3s, transform 0.3s;
+            }
+            &::before {
+              top: 0;
+              -webkit-transform: translateY(-10px);
+              -moz-transform: translateY(-10px);
+              transform: translateY(-10px);
+            }
+            &::after {
+              bottom: 0;
+              -webkit-transform: translateY(10px);
+              -moz-transform: translateY(10px);
+              transform: translateY(10px);
+            }
+            &:hover {
+              color: #fff;
+              cursor: pointer;
+              text-decoration: none;
+            }
+            &:visited {
+              text-decoration: none;
+            }
+            &:alink {
+              text-decoration: none;
+            }
+            &:active {
+              text-decoration: none;
+            }
+            &:focus {
+              color: #fff;
+            }
+            &:hover::before,&:focus::before,&:hover::after,&:focus::after {
+            opacity: 1;
+            -webkit-transform: translateY(0px);
+            -moz-transform: translateY(0px);
+            transform: translateY(0px);          
+            }
+          }
+        }        
       }
     }
   }
-
-</style>
+  </style>
